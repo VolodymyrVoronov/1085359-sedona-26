a50626d2152f9a08dfcaf26ff194e556a2598f21
Volodymyr <malones.vladimir@gmail.com>
2019-09-23 22:24:05 +0200

    Личный проект: начинаем оформление декоративных

---
+++ js/popup-hidden-show.js
@@ -0,0 +1,9 @@
+var	link = document.querySelector(".search__btn")
+
+var popup = document.querySelector(".search__form");
+var close = popup.querySelector(".search-form--visible");
+
+link.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.toggle("search-form--visible");
+});
