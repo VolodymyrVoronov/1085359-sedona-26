1b78527e5e4a1508e344c3061101ebf063bbcc54
Volodymyr <malones.vladimir@gmail.com>
2019-10-03 21:29:41 +0200

    Личный проект: оживляем интерактивные элементы
    Оживление формы.
    Анимация формы при появлении и при ошибке.

--- js/popup-hidden-show.js
+++ js/popup-hidden-show.js
@@ -1,9 +1,41 @@
-var	link = document.querySelector(".search__btn")
+var	link = document.querySelector(".search__btn");
+var form = document.querySelector(".search__form");
 
-var popup = document.querySelector(".search__form");
-var close = popup.querySelector(".search-form--visible");
+var dateOfArrival = form.querySelector("[name=date-of-arrival]");
+var dateOfDeparture = form.querySelector("[name=date-of-departure]");
+var amountOfAdults = form.querySelector("[name=amount-of-adults]");
+var amountOfChildren = form.querySelector("[name=amount-of-children]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("amountOfAdults", "amountOfChildren");
+    } catch (err) {
+    isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.toggle("search-form--visible");
+    form.classList.toggle("search-form--visible");
+    if (storage) {
+        amountOfAdults.value = storage;
+        amountOfChildren.focus();
+    } else {
+        amountOfAdults.focus();
+    }
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!dateOfArrival.value || !dateOfDeparture.value || !amountOfAdults.value || !amountOfChildren.value) {
+        evt.preventDefault();
+        form.classList.remove("search-form--error");
+        form.offsetWidth = form.offsetWidth;
+        form.classList.add("search-form--error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("amountOfAdults", amountOfAdults.value);
+            localStorage.setItem("amountOfChildren", amountOfAdults.value);
+        }
+    }
 });
