93dfdb7f90a4f1a8851c005a97397a50af853f3d
Volodymyr <malones.vladimir@gmail.com>
2019-10-06 16:40:24 +0200

    Личный проект: добавляем интерактивную карту
    Доработка проекта.

--- js/popup-hidden-show.js
+++ js/popup-hidden-show.js
@@ -1,13 +1,29 @@
 var	link = document.querySelector(".search__btn");
 var form = document.querySelector(".search__form");
 
+var send = form.querySelector(".search-form__btn");
 var dateOfArrival = form.querySelector("[name=date-of-arrival]");
 var dateOfDeparture = form.querySelector("[name=date-of-departure]");
 var amountOfAdults = form.querySelector("[name=amount-of-adults]");
 var amountOfChildren = form.querySelector("[name=amount-of-children]");
 
+var storage = {};
+
+storage.amountOfAdults = localStorage.getItem("amount-of-adults");
+storage.amountOfChildren = localStorage.getItem("amount-of-children");
+
+if (storage.amountOfAdults) {
+    amountOfAdults.value = storage.amountOfAdults;
+    amountOfChildren.focus();
+} 
+if (storage.amountOfChildren) {
+    amountOfChildren.value = storage.amountOfChildren;
+    send.focus();
+}
+
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
+    form.classList.remove("search-form--error");
     form.classList.toggle("search-form--invisible");
 });
 
@@ -17,5 +33,9 @@ form.addEventListener("submit", function (evt) {
         form.classList.remove("search-form--error");
         form.offsetWidth = form.offsetWidth;
         form.classList.add("search-form--error");
+    } else {
+        localStorage.setItem("amount-of-adults", amountOfAdults.value);
+        localStorage.setItem("amount-of-children", amountOfChildren.value);
+        
     }
 });
